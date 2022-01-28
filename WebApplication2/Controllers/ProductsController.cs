348b53a2a24c6e67a764abf779143c344044b32e
Francisco Funes <ffunes90@gmail.com>
2022-01-28 19:43:19 -0300

    Add project files.

---
+++ WebApplication2/Controllers/ProductsController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProductStore.Models;
+
+namespace ProductStore.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        /*Calling new ProductRepository() in the controller is not the best design, 
+         * because it ties the controller to a particular implementation of IProductRepository. 
+         * For a better approach, see Using the Web API Dependency Resolver.*/
+        static readonly IProductRepository repository = new ProductRepository();
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return repository.GetAll();
+        }
+
+        public Product GetProduct(int id)
+        {
+            Product item = repository.Get(id);
+            if (item == null)
+            {   
+                // Translated as a 404 (not found)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return item;
+        }
+
+        public IEnumerable<Product> GetProductByCategory(string category)
+        {
+            // LINQ for Where method
+            // Web API tries to match the query parameters to parameters on the controller method
+            return repository.GetAll().Where(
+                p => string.Equals(p.Category, category, StringComparison.OrdinalIgnoreCase));
+        }
+
+        //This example does not validate the Product. For information about model validation, see Model Validation in ASP.NET Web API.
+        public HttpResponseMessage PostProduct(Product item)
+        {
+            item = repository.Add(item);
+
+            //The CreateResponse method creates an HttpResponseMessage 
+            // Returning an HttpResponseMessage instead of a Product, we can control the details of the HTTP response message,
+            var response = Request.CreateResponse<Product>(HttpStatusCode.Created, item);
+
+            string uri = Url.Link("DefaultApi", new { id = item.Id });
+            response.Headers.Location = new Uri(uri);
+            return response;
+        }
+
+        //The method name starts with "Put...", so Web API matches it to PUT requests
+        // The method takes two parameters, the product ID and the updated product
+        public void PutProduct(int id, Product product)
+        {
+            product.Id = id;
+            if (!repository.Update(product))
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+
+        public void DeleteProduct(int id)
+        {
+            Product item = repository.Get(id);
+
+            if (item == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            repository.Remove(id);
+        }
+
+    }
+}
---
+++ WebApplication2/Models/ProductRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductStore.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+
+        // Create a list of products, using Product model as a type for List items
+        private List<Product> products = new List<Product>();
+        private int _nextId = 1;
+
+        public ProductRepository()
+        {
+            Add(new Product { Name = "Tomato soup", Category = "Groceries", Price = 1.39M });
+            Add(new Product { Name = "Yo-yo", Category = "Toys", Price = 3.75M });
+            Add(new Product { Name = "Hammer", Category = "Hardware", Price = 16.99M });
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return products;
+        }
+
+        public Product Add(Product item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int id)
+        {
+            return products.Find(p => p.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+           products.RemoveAll(p => p.Id == id);
+        }
+
+        public bool Update(Product item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("Add an item");
+            }
+            int index = products.FindIndex(p => p.Id == item.Id);
+            if (index == -1)
+            {
+                return false;
+            }
+            products.RemoveAt(index);
+            products.Add(item);
+            return true;
+        }
+    }
+}
